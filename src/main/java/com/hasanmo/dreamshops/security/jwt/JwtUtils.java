ea235daf99edf550fd94dc978c345ef75c52ba94
Hasanmourad <hasan.mourad8425@gmail.com>
2025-08-01 20:33:59 +0300

    add security part and login endpoint

---
+++ src/main/java/com/hasanmo/dreamshops/security/jwt/JwtUtils.java
@@ -0,0 +1,66 @@
+package com.hasanmo.dreamshops.security.jwt;
+
+import com.hasanmo.dreamshops.security.user.ShopUserDetails;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.security.SecureRandom;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class JwtUtils {
+
+    @Value("${auth.token.jwtSecret}")
+    private String jwtSecret;
+    @Value("${auth.token.expirationInMils}")
+    private int expirationTime;
+
+    public String generateTokenForUser(Authentication authentication)
+    {
+
+        ShopUserDetails userPrincipal = (ShopUserDetails) authentication.getPrincipal();
+        List<String> roles = userPrincipal.getAuthorities()
+                .stream().map(GrantedAuthority::getAuthority).toList();
+        return Jwts.builder()
+                .subject(userPrincipal.getUsername())
+                .claim("id",userPrincipal.getId())
+                .claim("roles", roles)
+                .issuedAt(new Date())
+                .expiration(new Date(new Date().getTime() + expirationTime))
+                .signWith(key(), SignatureAlgorithm.ES256).compact();
+    }
+
+    private Key key()
+    {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+    public String getUserNameFromToken(String token)
+    {
+        return Jwts.parser()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody().getSubject();
+    }
+    public  boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(key())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException |
+                 IllegalArgumentException e) {
+            throw new JwtException(e.getMessage());
+
+        }
+    }
+
+}
--- src/main/java/com/hasanmo/dreamshops/service/user/UserService.java
+++ src/main/java/com/hasanmo/dreamshops/service/user/UserService.java
@@ -9,6 +9,9 @@ import com.hasanmo.dreamshops.request.CreateUserRequest;
 import com.hasanmo.dreamshops.request.UserUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -19,6 +22,8 @@ public class UserService implements IUserService {
 
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
+
 
     @Override
     public User getUserById(Long userId) {
@@ -32,7 +37,7 @@ public class UserService implements IUserService {
                 .map(user -> {
                     User userNew = new User();
                     user.setEmail(request.getEmail());
-                    user.setPassword(request.getPassword());
+                    user.setPassword(passwordEncoder.encode(request.getPassword()));
                     user.setFirstName(request.getFirstName());
                     user.setLastName(request.getLastName());
                     return  userRepository.save(userNew);
@@ -58,5 +63,13 @@ public class UserService implements IUserService {
     @Override
     public UserDto convertUserToDto(User user){
         return modelMapper.map(user, UserDto.class);
+    }
+
+    @Override
+    public User getauthenticatedUser()
+    {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        return userRepository.findByEmail(email);
     }
 }
